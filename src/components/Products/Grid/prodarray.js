93fce5fd6b877c9ccbf95042cc83abbf388d3635
dsuekamling <dsuekamling@gmail.com>
2023-04-05 11:37:43 -0400

    Update prodarray.js

--- src/components/Products/Grid/prodarray.js
+++ src/components/Products/Grid/prodarray.js
@@ -1,72 +1,72 @@
 export const prodarray = [
 	{
-	  productName: "Black Deck Board",
+	  productName: "Pooh POP",
 	  productURL: "/productOne.jpg",
 	  productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget sapien vel ipsum venenatis sollicitudin.",
 	  price: '600'
 	},
 	{
-	  productName: "Wooden Surfer",
+	  productName: "JB POP",
 	  productURL: "/productTwo.jpg",
 	  productDesc: "Sed varius dolor quis lacus cursus, a hendrerit nulla rhoncus. Praesent quis magna vel arcu semper congue.",
 	  price: '400'
 	},
 	{
-	  productName: "Black and White",
+	  productName: "Micheal Jordan POP",
 	  productURL: "/productThree.jpg",
 	  productDesc: "Nam tincidunt purus quis elit volutpat, ac dignissim tellus malesuada. Sed consectetur quam eu velit.",
 	  price: '650'
 	},
 	{
-	  productName: "Blue Justice",
+	  productName: "Nerds POP",
 	  productURL: "/productFour.jpg",
 	  productDesc: "Vivamus id ligula ut arcu sodales pharetra ut ac velit. Maecenas bibendum, nunc ut tristique commodo.",
 	  price: '720'
 	},
 	{
-	  productName: "Striped Crusier",
+	  productName: "Sonic POP",
 	  productURL: "/productFive.jpg",
 	  productDesc: "Pellentesque commodo orci vel purus interdum, sed fringilla mauris eleifend. Donec eget vestibulum purus.",
 	  price: '515'
 	},
 	{
-	  productName: "Red And Black",
+	  productName: "Superman POP",
 	  productURL: "/productSix.jpg",
 	  productDesc: "Donec sit amet arcu vitae mauris suscipit luctus in eu neque. Fusce eget tortor ac diam.",
 	  price: '40'
 	},
 	{
-	  productName: "Surfing Tee",
+	  productName: "Ghostface POP",
 	  productURL: "/productSeven.jpg",
 	  productDesc: "Fusce pharetra interdum felis, vel iaculis turpis accumsan ac. Vestibulum at nunc ut nunc commodo.",
 	  price: '30'
 	},
 	{
-	  productName: "Sleeved Tee",
+	  productName: "Spidy POP",
 	  productURL: "/productEight.jpg",
 	  productDesc: "Sed commodo tincidunt orci, eget euismod leo dapibus vel. In viverra varius nulla, quis tincidunt velit.",
 	  price: '35'
 	},
 	{
-	  productName: "Croc Top",
+	  productName: "Eddie POP",
 	  productURL: "/productNine.jpg",
 	  productDesc: "Quisque feugiat orci eu diam lobortis bibendum. Morbi a lobortis elit. Praesent et elit ut nisl pretium.",
 	  price: '45'
 	},
 	{
-	  productName: "Surfers Kicks",
+	  productName: "MHA POP",
 	  productURL: "/productTen.jpg",
 	  productDesc: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla euismod cursus purus.",
 	  price: '68'
 	},
 	{
-		productName: "Yellow Vans",
+		productName: "Psycho POP",
 		productURL: "/productEleven.jpg",
 		productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nulla at dignissim iaculis.",
 		price: '52'
 	  },
 	  {
-		productName: "Slip on Summer",
+		productName: "One Piece POP",
 		productURL: "/productTwelve.jpg",
 		productDesc: "Pellentesque ac lacus ut ipsum vestibulum maximus. Ut ut tristique massa. Mauris in est vel est semper ultricies.",
 		price: '63'
