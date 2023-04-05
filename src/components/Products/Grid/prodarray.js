4805edd87e374cab90380b0523787de9c653b2f2
dsuekamling <dsuekamling@gmail.com>
2023-04-05 11:58:59 -0400

    Update prodarray.js

--- src/components/Products/Grid/prodarray.js
+++ src/components/Products/Grid/prodarray.js
@@ -2,73 +2,73 @@ export const prodarray = [
 	{
 	  productName: "Pooh POP",
 	  productURL: "/productOne.jpg",
-	  productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget sapien vel ipsum venenatis sollicitudin.",
-	  price: '600'
+	  productDesc: "This Funko Pop captures the essence of your favorite character with its cute and charming design",
+	  price: '60'
 	},
 	{
 	  productName: "JB POP",
 	  productURL: "/productTwo.jpg",
-	  productDesc: "Sed varius dolor quis lacus cursus, a hendrerit nulla rhoncus. Praesent quis magna vel arcu semper congue.",
-	  price: '400'
+	  productDesc: "Add this adorable Funko Pop to your collection and bring your favorite character to life on your shelf.",
+	  price: '40'
 	},
 	{
 	  productName: "Micheal Jordan POP",
 	  productURL: "/productThree.jpg",
-	  productDesc: "Nam tincidunt purus quis elit volutpat, ac dignissim tellus malesuada. Sed consectetur quam eu velit.",
-	  price: '650'
+	  productDesc: "This Funko Pop is the perfect gift for any fan of the franchise, with its detailed design and vibrant colors.",
+	  price: '65'
 	},
 	{
 	  productName: "Nerds POP",
 	  productURL: "/productFour.jpg",
-	  productDesc: "Vivamus id ligula ut arcu sodales pharetra ut ac velit. Maecenas bibendum, nunc ut tristique commodo.",
-	  price: '720'
+	  productDesc: "Celebrate your love for the series with this Funko Pop, featuring your favorite character in their iconic pose.",
+	  price: '72'
 	},
 	{
 	  productName: "Sonic POP",
 	  productURL: "/productFive.jpg",
-	  productDesc: "Pellentesque commodo orci vel purus interdum, sed fringilla mauris eleifend. Donec eget vestibulum purus.",
-	  price: '515'
+	  productDesc: "With its intricate details and high-quality design, this Funko Pop is a must-have for any true fan.",
+	  price: '51'
 	},
 	{
 	  productName: "Superman POP",
 	  productURL: "/productSix.jpg",
-	  productDesc: "Donec sit amet arcu vitae mauris suscipit luctus in eu neque. Fusce eget tortor ac diam.",
+	  productDesc: "Display this Funko Pop on your desk or shelf and show off your love for the character to everyone who visits.",
 	  price: '40'
 	},
 	{
 	  productName: "Ghostface POP",
 	  productURL: "/productSeven.jpg",
-	  productDesc: "Fusce pharetra interdum felis, vel iaculis turpis accumsan ac. Vestibulum at nunc ut nunc commodo.",
+	  productDesc: "This Funko Pop is a great addition to any collection, with its unique design and eye-catching colors.",
 	  price: '30'
 	},
 	{
 	  productName: "Spidy POP",
 	  productURL: "/productEight.jpg",
-	  productDesc: "Sed commodo tincidunt orci, eget euismod leo dapibus vel. In viverra varius nulla, quis tincidunt velit.",
+	  productDesc: "Get ready to embark on new adventures with this Funko Pop, featuring your favorite character ready for action.",
 	  price: '35'
 	},
 	{
 	  productName: "Eddie POP",
 	  productURL: "/productNine.jpg",
-	  productDesc: "Quisque feugiat orci eu diam lobortis bibendum. Morbi a lobortis elit. Praesent et elit ut nisl pretium.",
+	  productDesc: "This Funko Pop is the perfect way to bring your favorite character into your daily life, whether at home or at work.",
 	  price: '45'
 	},
 	{
 	  productName: "MHA POP",
 	  productURL: "/productTen.jpg",
-	  productDesc: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla euismod cursus purus.",
+	  productDesc: "With its fun and whimsical design, this Funko Pop is sure to bring a smile to your face every time you see it.",
 	  price: '68'
 	},
 	{
 		productName: "Psycho POP",
 		productURL: "/productEleven.jpg",
-		productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nulla at dignissim iaculis.",
+		productDesc: "This Funko Pop captures the essence of the character in its playful design and striking colors.",
 		price: '52'
 	  },
 	  {
 		productName: "One Piece POP",
 		productURL: "/productTwelve.jpg",
-		productDesc: "Pellentesque ac lacus ut ipsum vestibulum maximus. Ut ut tristique massa. Mauris in est vel est semper ultricies.",
+		productDesc: "Take your fandom to the next level with this Funko Pop, featuring your favorite character in their most iconic outfit.",
 		price: '63'
 	  }
 	]	  
